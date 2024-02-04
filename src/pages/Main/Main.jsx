6c5568fcf2aa187687710512c22d0c858cab43a3
AZ0228 <jbliu88@gmail.com>
2024-02-04 13:25:21 -0500

    added popup functionality

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 // import Register from '../components/Forms/Register/Register';
 import Header from '../components/Header/Header';
 import './Main.css';
 import calendar from '../../assets/dummycal.png';
+import Popup from '../components/Popup/Popup';
 
 function Main(){
+
+    const [showPopup, setShowPopup] = useState(false);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setShowPopup(true);
+        }, 1000);
+    }, [showPopup]);
+
     return(
         <div className="main">
             <Header />
             <div className="main-container">
-                <img src={calendar} alt="calendar"></img>
+                <img src={calendar} alt="calendar" className="calendar"></img>
             </div>
-
+            <div className={`smokescreen ${showPopup ? "activePopup": ""}`}>
+                <Popup />
+            </div>
         </div>
     );
 }
